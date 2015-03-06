86d9572129c40f957cc898da44980d87ad1f7fee
MuTanClanRobo <jsdcrobo17@gmail.com>
2015-03-05 23:13:46 -0600

    update tcpconnect.py 3/5/15
    This code is working to get real time data from the GUI. The next step is to change the GUI so that it works for our robot and then write the pin outputs. :)

--- tcpconnect.py
+++ tcpconnect.py
@@ -1,31 +1,31 @@
-#!/usr/bin/env python
-
 import socket
 import json
+import time
+from termios import tcflush, TCIFLUSH
+import sys
 
 TCP_IP = '192.168.7.2'
 TCP_PORT = 5005
-BUFFER_SIZE = 256  # Normally 1024, but we want fast response
+BUFFER_SIZE = 512  # Normally 1024, but we want fast response
 
-
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
-
-conn, addr = s.accept()
-print 'Connection address:', addr
-data = ""
 while 1:
-    while len(data) == 0:
-        data = data + conn.recv(BUFFER_SIZE)
-    print "received data:", data
-#    controllerdata = json.loads(str(data)) #Assume that data is the JSON string which we just got from our 
-#    print "this is the data", controllerdata
-    #if not data: break
-    #conn.send(data)  # echo
-    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
+    s.listen(1)
 
+    conn, addr = s.accept()
+    print 'Connection address:', addr
+    while 1:
+        data = conn.recv(BUFFER_SIZE)
+        if not data: break
+        #print "received data:", data
+        controllerdata = json.loads(str(data))
+        for k,v in controllerdata.items(): 
+            print str(k) + " " + str(v)
+        #print "controllerdata:", controllerdata
+        conn.send(data)  # echo
+    conn.close()
+    time.sleep(.001)
+    tcflush(sys.stdin, TCIFLUSH)
+    #digitalWrite(, elavtorArm)
 
-
-#conn.close()
